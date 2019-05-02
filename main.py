528fb42f6155be0a4f6d465f6c7d2e5f5ebc0cfa
Piotr Masztalski <masztalski.piotr@gmail.com>
2019-05-02 18:55:31 +0200

    "train test split added"

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.utils import shuffle
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 from network import Network, Layer
 
 
@@ -16,9 +17,12 @@ if __name__ == "__main__":
     data = iris_data.data
     labels = iris_data.target
     n_records, n_features = data.shape
-    normalized_data = MinMaxScaler().fit_transform(data)
 
+    normalized_data = MinMaxScaler().fit_transform(data)
     labels = np.array(pd.get_dummies(labels))
+    train_data, test_data, train_labels, test_labels = train_test_split(
+        normalized_data, labels, shuffle=True, test_size=0.2)
+
     n_output_neurons = labels[1].shape[0]
 
     # network construction
@@ -37,9 +41,9 @@ if __name__ == "__main__":
     for e in range(epochs):
         if (e + 1) % 100 == 0:
             print("Epoch: ", e+1)
-        for i in range(len(labels)):
-            output = net.forward(normalized_data[i])
-            error = error_formula(output=np.array(output), target=np.array(labels[i]))
+        for i in range(len(train_labels)):
+            output = net.forward(train_data[i])
+            error = error_formula(output=np.array(output), target=np.array(train_labels[i]))
             net.backward(error)
             net.update_weights()
 
@@ -47,10 +51,9 @@ if __name__ == "__main__":
 
     correct = 0
 
-    for i in range(len(labels)):
-        correct += int(np.argmax(net.forward(normalized_data[i])) == np.argmax(labels[i]))
-        print(np.argmax(net.forward(normalized_data[i])) == np.argmax(labels[i]))
+    for i in range(len(test_labels)):
+        correct += int(np.argmax(net.forward(test_data[i])) == np.argmax(test_labels[i]))
 
-    print("Accuracy = {:.3f}".format(correct/len(labels)))
+    print("Accuracy = {:.3f}".format(correct/len(test_labels)))
 
     # TODO: division to training and testing set
